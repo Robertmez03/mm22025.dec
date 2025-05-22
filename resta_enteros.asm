a4b190b2afde437bf995becd333e26b4163743fd
Roberto Mena <mm22025@ues.edu.sv>
2025-05-22 09:46:06 -0600

    Solucion Ejercicio 1: Resta de 3 enteros usando registros de 16 bits(Nativo)

--- resta_enteros.asm
+++ resta_enteros.asm
@@ -1,15 +1,70 @@
 ; Programa: Resta de tres enteros (16 bits)
 ; Autor: Roberto Mena - MM22025
+; resta_enteros.asm
+; nasm -f elf32 resta_enteros.asm -o resta_enteros.o
+; ld  -m elf_i386 resta_enteros.o -o resta_enteros
 
-org 100h           ; Punto de entrada para DOS (modo real)
+bits 32
+
+section .data
+  num1   dw 150      ; primer operando 16 bits
+  num2   dw  30      ; segundo
+  num3   dw  20      ; tercero
+  result dw  0       ; aquí guardamos AX
+
+  prefix db "Result: ",0
+  buf    db "00000",10   ; espaciamos 5 dígitos + '\n'
 
 section .text
-start:
-    mov ax, 50     ; Primer número
-    sub ax, 20     ; Resta el segundo
-    sub ax, 10     ; Resta el tercero
+  global _start
 
-    ; El resultado queda en AX
+_start:
+  ; 1) resta en 16 bits
+  mov  ax, [num1]   ; AX = num1
+  mov  bx, [num2]   ; BX = num2
+  sub  ax, bx       ; AX = AX – BX
+  mov  cx, [num3]   ; CX = num3
+  sub  ax, cx       ; AX = AX – CX
+  mov  [result], ax
 
-    mov ah, 0x4C   ; Función para salir del programa en DOS
-    int 21h
+  ; 2) imprimir "Result: "
+  mov  eax,4
+  mov  ebx,1
+  mov  ecx,prefix
+  mov  edx,8
+  int 0x80
+
+  ; 3) convertir [result] a ASCII decimal, 5 dígitos
+  movzx eax, word [result]  ; extender a 32 bits
+  lea   edi, [buf+4]        ; puntero al último dígito
+  mov   ecx, 5
+.conv:
+  xor   edx,edx
+  mov   ebx,10
+  div   ebx                 ; EAX=EAX/10, EDX=resto
+  add   dl,'0'
+  mov   [edi], dl
+  dec   edi
+  dec   ecx
+  test  eax,eax
+  jnz   .conv
+.pad:
+  cmp   ecx,0
+  je    .donepad
+  mov   byte [edi],'0'
+  dec   edi
+  dec   ecx
+  jmp   .pad
+.donepad:
+
+  ; 4) imprimir buf (5 dígitos + '\n')
+  mov  eax,4
+  mov  ebx,1
+  mov  ecx,buf
+  mov  edx,6
+  int 0x80
+
+  ; 5) salir
+  mov  eax,1
+  xor  ebx,ebx
+  int 0x80
